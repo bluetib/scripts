919a9e8a26b79837d64bb3d19f0f0d8c2cafeb05
StarCloud <liu.xiao.hu.net@gmail.com>
2016-11-23 10:38:02 +0800

    fix bug

--- git_pull_for_all_project_here.sh
+++ git_pull_for_all_project_here.sh
@@ -21,16 +21,29 @@ do
 	if [ "$branch_name" != "" ];then
 		N=$(git branch -a|egrep -v remote|sed 's/*//'|column -t|egrep ${branch_name}|wc -l)
 		if [ $N -eq 1 ];then
+			echo -e "=== checkout to $branch_name to update this branch $branch_name ==="
 			git checkout $branch_name
+			git br -a
 		else
-			git checkout -b ${branch_name} origin/${branch_name}
+			N1=$(git branch -a|egrep remote|column -t|egrep ${branch_name}|wc -l)
+			if [ $N1 -eq 1 ];then
+				git checkout -b ${branch_name} origin/${branch_name}
+			else
+				echo -e "Sorry. no remote branch name match your request [$branch_name]"
+				cd ..
+				continue
+			fi
 		fi
 	fi
+	echo -e "--- git pulling for branch $branch_name ---"
 	git pull 2>/dev/null
 	date2=$(date +%s)
 	time_used=$((date2-date1))
-	echo -e "|||| checkout to dev |||||"
-	git checkout dev
+	N2=$(git branch -a|egrep -v remote|sed 's/*//'|column -t|egrep "dev"|wc -l)
+	if [ $N2 -eq 1 ];then
+		echo -e " ---- checkout to dev ----"
+		git checkout dev
+	fi
 	echo -e "===================================== Pull used time seconds: [$time_used] ===========================================\n"
 	cd ..
 done
