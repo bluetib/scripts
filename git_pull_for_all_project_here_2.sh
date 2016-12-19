6444aebd18e0c1846d7165151e298e77bc82f95c
StarCloud <liu.xiao.hu.net@gmail.com>
2016-12-19 11:28:32 +0800

    added another git_pull script for git pull --all

---
+++ git_pull_for_all_project_here_2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+now_path=`cd $(dirname $0) && pwd`
+cd $now_path
+
+for i in `ls -al|egrep ^d|egrep -v '\.$'|awk '{print $NF}'`
+do
+	date1=$(date +%s)
+	cd $i
+	if [ ! -d "./.git" ];then
+		cd ..
+		continue
+	fi
+	echo -e "======================================= $i ========================================="
+	echo -e "I am git pulling all branch from all remote.. Please just wait for a while.. "
+	sleep 1
+	git pull --all
+	echo -e "===================================== Pull used time seconds: [$time_used] ===========================================\n"
+	cd ..
+done
