3d386081a97e121bcb9f16adfdc7548e0981518e
StarCloud <liu.xiao.hu.net@gmail.com>
2017-07-04 21:58:42 +0800

    fix  and 优化

--- Show_unpushed_rep.sh
+++ Show_unpushed_rep.sh
@@ -11,10 +11,48 @@ else
 fi
 
 if [ $# -eq 1 ];then
-    branch_name="$1" else
+    branch_name="$1"
+else
     branch_name=""
 fi
 
+function check_branch_exist()
+{
+    local branch_name="$1"
+    N=$(git branch |sed "s+*++g"|column -t|egrep "^${branch_name}$"|wc -l)
+    if [ $N -eq 1 ];then
+        echo -e "yes"
+    else
+        echo -e "no"
+    fi
+}
+
+function get_default_branch_if_need()
+{
+    num=$(git branch -a|egrep -v "origin"|egrep '^\*'|sed 's/* //'|column -t|wc -l)
+    if [ $num -eq 1 ];then
+        default_branch_name=$(git branch -a|egrep -v "remotes"|egrep '^\*'|sed 's/* //'|column -t)
+        echo -e "$default_branch_name"
+    else
+        echo -e "no"
+    fi
+}
+
+function get_remote_info_by_branch_name()
+{
+    local branch_name="$1"
+    N1=$(git branch -a|egrep "$branch_name" -w|egrep remotes|egrep -v HEAD|wc -l)
+    if [ $N1 -gt 1 ];then
+        echo -e "+++++"
+        git remote -v
+        echo -e "+++++"
+        return 1
+    fi
+    remote=$(git branch -a|egrep "$branch_name" -w|egrep remotes|egrep -v HEAD|awk -F'/' '{print $2}')
+    echo -e "+++++"
+    git remote -v|egrep "^${remote}"
+    echo -e "+++++"
+}
 function check_if_ahead()
 {
     local branch_name="$1"
@@ -25,17 +63,6 @@ function check_if_ahead()
         else
             echo -e "=== |||||||||||||| =========== Please check [Need To Push] ahead of $ahead_num commits ============|||||||||||||| ==="
         fi
-    fi
-}
-
-function check_branch_exist()
-{
-    local branch_name="$1"
-    N=$(git branch |sed "s+*++g"|column -t|egrep -w "^${branch_name}$"|wc -l)
-    if [ $N -eq 1 ];then
-        echo -e "yes"
-    else
-        echo -e "no"
     fi
 }
 
@@ -56,9 +83,11 @@ if [ -f "rep_list" ];then
             continue
         fi
         echo -e "========================================== $rep ============================================="
+        no_branch_match="yes"
         for i in $branchs
         do
             if [ $(check_branch_exist $i) = "yes" ];then
+                no_branch_match="no"
                 git checkout $i
                 check_if_ahead $i
                 git status
@@ -68,6 +97,15 @@ if [ -f "rep_list" ];then
                 continue
             fi
         done
+        if [ "$no_branch_match" == "yes" ];then
+            default_branch=$(get_default_branch_if_need)
+        fi
+        if [ "$default_branch" != "no" ];then
+            git checkout $default_branch
+            check_if_ahead $i
+            git status
+            echo -e "-------------------"
+        fi
         N2=$(git branch -a|egrep -v remote|sed 's/*//'|column -t|egrep -w "$last_checkout_to_branch"|wc -l)
         if [ $N2 -eq 1 ];then
             echo -e "--- [checkout to $last_checkout_to_branch] ---"
@@ -109,7 +147,8 @@ do
         check_if_ahead "$branch_name"
         git status
     else
-        check_if_ahead "dev"
+        default_branch=$(get_default_branch_if_need)
+        check_if_ahead $default_branch
         git status
     fi
     N2=$(git branch -a|egrep -v remote|sed 's/*//'|column -t|egrep -w "dev"|wc -l)
