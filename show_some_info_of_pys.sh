61c6adeb3bd1d240748e431838da1e921f69da97
StarCloud <liu.xiao.hu.net@gmail.com>
2017-11-08 17:02:30 +0800

    added sh

--- show_some_info_of_pys.sh
+++ show_some_info_of_pys.sh
@@ -15,8 +15,8 @@ function show_info()
 {
     local num="$1"
     local message="$2"
-    echo -e "$num、 --- [$message] ---\n"
-    sleep 1
+    echo -e "\n$num、 --- [$message] ---\n"
+    sleep 0.5
 }
 
 function show_info_2()
@@ -25,7 +25,7 @@ function show_info_2()
     echo -e "======================"
     echo -e "$message"
     echo -e "======================\n"
-    sleep 1
+    sleep 0.5
 }
 
 function clean()
@@ -48,22 +48,59 @@ if [ ! -d "$t_dir" ];then
 fi
 t_file_1="$t_dir/t_file_1"
 t_file_2="$t_dir/t_file_2"
+t_file_3="$t_dir/t_file_3"
+t_file_4="$t_dir/t_file_4"
 #---------------
 
 find $the_path -name "*.py" -type f > $t_file_1
-file_num=$(cat $t_file_1|wc -l)
-show_info_2 "This time I got $file_num files"
+find $the_path -name "*.sh" -type f > $t_file_4
+py_file_num=$(cat $t_file_1|wc -l)
+sh_file_num=$(cat $t_file_4|wc -l)
+show_info_2 "This time I got $py_file_num python files"
+show_info_2 "This time I got $sh_file_num shell files"
 show_info 1 "calculate every file"
-sum=0
+sum_py=0
 for i in `cat $t_file_1`
 do
     N=$(cat $i|wc -l)
-    sum=$((sum+N))
+    sum_py=$((sum_py+N))
     echo -e "$N $i" >> $t_file_2
 done
+sum_sh=0
+for i in `cat $t_file_4`
+do
+    N=$(cat $i|wc -l)
+    sum_sh=$((sum_sh+N))
+    echo -e "$N $i" >> $t_file_3
+done
 
-show_info 2 "show every file info"
-cat $t_file_2|sort -n -k1
-show_info_2 "All code lines is [$sum]"
+if [ $sh_file_num -ge 10000 ];then
+    echo -e "So many files.. You sure want to check this ?"
+    read -p "[yes/no/y/n]: " choice
+    if [ "$choice" == "yes" -o "$choice" == "y" -o "$choice" == "" -o "$choice" == "Y" ];then
+        show_info 2 "show every shell file info"
+        cat $t_file_3|sort -n -k1
+    else
+        echo -e "OK.No show this time..If you want to see please run script again."
+    fi
+else
+    show_info 2 "show every shell file info"
+    cat $t_file_3|sort -n -k1
+fi
+if [ $py_file_num -ge 10000 ];then
+    echo -e "So many files.. You sure want to check this ?"
+    read -p "[yes/no/y/n]: " choice
+    if [ "$choice" == "yes" -o "$choice" == "y" -o "$choice" == "" -o "$choice" == "Y" ];then
+        show_info 2 "show every python file info"
+        cat $t_file_2|sort -n -k1
+    else
+        echo -e "OK.No show this time..If you want to see please run script again."
+    fi
+else
+    show_info 2 "show every python file info"
+    cat $t_file_2|sort -n -k1
+fi
+show_info_2 "All python code lines is [$sum_py]"
+show_info_2 "All shell code lines is [$sum_sh]"
 
 clean
