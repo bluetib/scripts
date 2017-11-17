4757595ab527f807182157ea92eea4c8fbd6a825
StarCloud <liu.xiao.hu.net@gmail.com>
2017-11-17 09:32:18 +0800

    up polipo.sh

--- useful_script/polipo.sh
+++ useful_script/polipo.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-sudo polipo proxyAddress=0.0.0.0 socksParentProxy="127.0.0.1:1090"  socksProxyType=socks5
+if [ $# -ne 4 ];then
+    echo -e "Usage:\n\t$0 \"polipo_listen_ip\" \"polipo_listen_port\" \"socksProxyIP\" \"socksProxyPort\" "
+    exit 12
+else
+    ip="$1"
+    port="$2"
+    ip2="$3"
+    port2="$4"
+fi
+
+ulimit -n 1000000
+
+echo -e "polipo proxyAddress=$ip proxyPort=$port socksParentProxy="${ip2}:${port2}"  socksProxyType=socks5 authCredentials=tib:gansiduiyo"
+polipo proxyAddress=$ip proxyPort=$port socksParentProxy="${ip2}:${port2}"  socksProxyType=socks5 authCredentials=tib:gansiduiyo
